7fbeedd5ba7833755c322d8b1e98a220924df367
Thomas Girotti <0022680701v@gmail.com>
2022-05-23 23:23:19 +0200

    Ajout des options de lancement

--- main.c
+++ main.c
@@ -1,32 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 
-//Test Commit n°1
+#define OPTSTR "h:b:c:d:e:f:i:l:g:o:r:x:s:Z:z"
 
-int main(int argc, char const *argv[]) {
-    char* filename;
-    filename = malloc(100 * sizeof(char));
-    
-    for (size_t i = 0; i < argc; i++) {
-         switch (argv[i]) {
-             case "-i" :
-                filename = argv[i+1];
+int main(int argc, char *argv[]) {
+    int opt;
+    while ((opt = getopt(argc, argv, OPTSTR)) != EOF) {
+        switch(opt) {
+            case 'h':
+                printf("AIDE POUR LE PROGRAMME");
                 break;
-             case 
-        
-    if (argv[n] == "-i")
-        argv[1]
-    filename = realloc(filename, strlen(filename) * sizeof(char));
+            case 'b':
+                printf("-b ENTERED");
+                break;
+            case 'c':
+                printf("-c ENTERED");
+                break;
+            case 'd':
+                printf("-d ENTERED");
+                break;
+            case 'e':
+                printf("-e ENTERED");
+                break;
+            case 'f':
+                printf("-f ENTERED");
+                break;
+            case 'i':
+                printf("-i ENTERED");
+                break;
+            case 'l':
+                printf("-l ENTERED");
+                break;
+            case 'g':
+                printf("-g ENTERED");
+                break;
+            case 'o':
+                printf("-o ENTERED");
+                break;
+            case 'r':
+                printf("-r ENTERED");
+                break;
+            case 'x':
+                printf("-x ENTERED");
+                break;
+            case 's':
+                printf("-s ENTERED");
+                break;
+            case 'Z':
+                printf("-Z ENTERED");
+                break;
+            case 'z':
+                printf("-z ENTERED");
+                break;
+            default:
+                printf("Mauvais argument entré ! \n");
+                break;
+        }
+    }
     return 0;
 }
 
-int openfile(char* filename) {
+
+/*int openfile(char* filename){
     FILE* file = NULL;
     char* currentligne[];
     
-    file = fopen("argv[1]
+    file = fopen("argv[1]","r")
     if (file == NULL) {
         printf("Erreur d'ouverture du fichier \n");
     } else {
         while (
+    }
+}*/

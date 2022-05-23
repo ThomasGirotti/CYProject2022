f31da80ab2347035080c1a92fbfbd31830a471d1
Lilian <106081036+Sulfy6768@users.noreply.github.com>
2022-05-23 15:14:48 +0200

    Update binaire.c

--- binaire.c
+++ binaire.c
@@ -1,9 +1,13 @@
 void binaire(image img,int seuil){
-  int pixel=0; 
+  int pixel=0;
+  int x;
+  int y;
+  x=img.x;
+  y=img.y;
   for(int i=0; i<x;i++){ // on balye les lignes
     for(int j=0; j<y;j++){ // on baleye les collones
       if (((img.pixel.red[x][y]+img.pixel.green[x][y]+img.pixel.blue[x][y])/3)<seuil){
-        img.pixel.red[x][y]=0; // transforme le rouge dans la bonne teinte
+        img.pixel.red[x][y]=0; // transforme le rouge en 
         img.pixel.green[x][y]=0; // transforme le vert dans la bonne teinte
         img.pixel.blue[x][y]=0; // transforme le bleu dans la bonne teinte
       }

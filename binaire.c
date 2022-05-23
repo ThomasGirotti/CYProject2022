7598fd3d8c158724630c605f2a628e0dc6848ca9
Lilian <106081036+Sulfy6768@users.noreply.github.com>
2022-05-23 15:19:01 +0200

    Update binaire.c

--- binaire.c
+++ binaire.c
@@ -6,15 +6,15 @@ void binaire(image img,int seuil){
   y=img.y;
   for(int i=0; i<x;i++){ // on balye les lignes
     for(int j=0; j<y;j++){ // on baleye les collones
-      if (((img.pixel.red[x][y]+img.pixel.green[x][y]+img.pixel.blue[x][y])/3)<seuil){
-        img.pixel.red[x][y]=0; // transforme le rouge en 
-        img.pixel.green[x][y]=0; // transforme le vert dans la bonne teinte
-        img.pixel.blue[x][y]=0; // transforme le bleu dans la bonne teinte
+      if (((img.pixel.red[i][j]+img.pixel.green[i][j]+img.pixel.blue[i][j])/3)<seuil){
+        img.pixel.red[i][j]=0; // transforme le rouge en 
+        img.pixel.green[i][j]=0; // transforme le vert dans la bonne teinte
+        img.pixel.blue[i][j]=0; // transforme le bleu dans la bonne teinte
       }
       else{
-        img.pixel.red[x][y]=255; // transforme le rouge dans la bonne teinte
-        img.pixel.green[x][y]=255; // transforme le vert dans la bonne teinte
-        img.pixel.blue[x][y]=255; // transforme le bleu dans la bonne teinte   
+        img.pixel.red[i][j]=255; // transforme le rouge dans la bonne teinte
+        img.pixel.green[i][j]=255; // transforme le vert dans la bonne teinte
+        img.pixel.blue[i][j]=255; // transforme le bleu dans la bonne teinte   
       }
     }
   }  

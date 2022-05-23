b5f158a6f61981e66348dde561ff3f71e77bf020
Lilian <106081036+Sulfy6768@users.noreply.github.com>
2022-05-23 15:17:59 +0200

    Update transform_gris.c

--- transform_gris.c
+++ transform_gris.c
@@ -1,11 +1,15 @@
 void transform_gris(image img){
-  int pixel=0; 
+  int pixel=0;
+  int x;
+  int y;
+  x=img.x;
+  y=img.y;
   for(int i=0; i<x;i++){ // on balye les lignes
     for(int j=0; j<y;j++){ // on baleye les collones
-      pixel=img.red[img.x][img.y]*0.2126+img.green[img.x][img.y]*0.7152+img.blu[img.x][img.y]*0.0722; // application de la formule
-      img.pixel.red[x][y]=pixel; // transforme le rouge dans la bonne teinte
-      img.pixel.green[x][y]=pixel; // transforme le vert dans la bonne teinte
-      img.pixel.blue[x][y]=pixel; // transforme le bleu dans la bonne teinte
+      pixel=img.red[i][j]*0.2126+img.green[i][j]*0.7152+img.blu[i][j]*0.0722; // application de la formule
+      img.pixel.red[i][j]=pixel; // transforme le rouge dans la bonne teinte
+      img.pixel.green[i][j]=pixel; // transforme le vert dans la bonne teinte
+      img.pixel.blue[i][j]=pixel; // transforme le bleu dans la bonne teinte
     }
   }  
 }

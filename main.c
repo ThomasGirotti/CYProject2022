fe970d8c1cdd65b0925a0dd757cd5c7eb52a314c
Thomas Girotti <32798532+ThomasGirotti@users.noreply.github.com>
2022-05-23 15:28:00 +0200

    Start main.c
    Début du main.c

--- main.c
+++ main.c
@@ -4,8 +4,29 @@
 
 //Test Commit n°1
 
-int main(int argc, char const *argv[])
-{
-    printf("Hello world!\n");
+int main(int argc, char const *argv[]) {
+    char* filename;
+    filename = malloc(100 * sizeof(char));
+    
+    for (size_t i = 0; i < argc; i++) {
+         switch (argv[i]) {
+             case "-i" :
+                filename = argv[i+1];
+                break;
+             case 
+        
+    if (argv[n] == "-i")
+        argv[1]
+    filename = realloc(filename, strlen(filename) * sizeof(char));
     return 0;
 }
+
+int openfile(char* filename) {
+    FILE* file = NULL;
+    char* currentligne[];
+    
+    file = fopen("argv[1]
+    if (file == NULL) {
+        printf("Erreur d'ouverture du fichier \n");
+    } else {
+        while (

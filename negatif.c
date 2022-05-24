59e179a82defe2ace4d65cc9dbba7c19925436ee
Lilian <106081036+Sulfy6768@users.noreply.github.com>
2022-05-24 14:18:13 +0200

    Update negatif.c

--- negatif.c
+++ negatif.c
@@ -1,1 +1,14 @@
-
+void negatif(image img){
+  int x;
+  int y;
+  x=img.x;
+  y=img.y;
+  for(int i=0; i<x;i++){ // on balye les lignes
+    for(int j=0; j<y;j++){ // on baleye les collones
+      img.pixel.red[i][j]= 255-img.pixel.red[i][j];
+      img.pixel.green[i][j]= 255-img.pixel.green[i][j];
+      img.pixel.blue[i][j]= 255-img.pixel.blue[i][j];
+      }
+    }
+  }  
+}

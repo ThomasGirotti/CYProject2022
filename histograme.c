5597ca16c2e37500afa00a9cf0f2b2b6b6d98c30
Lilian <106081036+Sulfy6768@users.noreply.github.com>
2022-05-24 14:28:36 +0200

    Creation de histograme.c

---
+++ histograme.c
@@ -0,0 +1,13 @@
+void histograme(image img){
+  int tab[255];
+  int x;
+  int y;
+  x=img.x;
+  y=img.y;
+  for(int i=0; i<x;i++){ // on balye les lignes
+    for(int j=0; j<y;j++){ // on baleye les collones
+      tab[img.pixel.red[i][j]]=+1
+      }
+    }
+  }  
+}

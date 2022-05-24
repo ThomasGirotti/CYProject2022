4904d3b5d86fa8a60f482fb7164f1361fe861122
Pierre-Antoine <103061131+Sou-race@users.noreply.github.com>
2022-05-24 15:09:13 +0200

    Fix tab rgb into statique

--- fonction.h
+++ fonction.h
@@ -3,10 +3,10 @@ typedef struct image{
   int x;
   int y;
   pix pixel;
-}
+} 
 
 typedef struct pix{
-  int** red[][];
-  int** green[][];
-  int** blue[][];
+  int red[x][y];
+  int green[x][y];
+  int blue[x][y];
 }

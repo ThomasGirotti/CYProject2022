4242d81b9734aec28e23ff31b57b8c48da54027a
Pierre-Antoine <103061131+Sou-race@users.noreply.github.com>
2022-05-24 15:27:04 +0200

    correction faute commentaire

--- negatif.c
+++ negatif.c
@@ -3,8 +3,8 @@ void negatif(image img){
   int y;
   x=img.x;
   y=img.y;
-  for(int i=0; i<x;i++){ // on balye les lignes
-    for(int j=0; j<y;j++){ // on baleye les collones
+  for(int i=0; i<x;i++){ // on balaie les lignes
+    for(int j=0; j<y;j++){ // on balaie les colonnes
       img.pixel.red[i][j]= 255-img.pixel.red[i][j];
       img.pixel.green[i][j]= 255-img.pixel.green[i][j];
       img.pixel.blue[i][j]= 255-img.pixel.blue[i][j];

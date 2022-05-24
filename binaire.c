6f5336f14c9ffc4665796e640450d3297807aecf
Pierre-Antoine <103061131+Sou-race@users.noreply.github.com>
2022-05-24 15:24:52 +0200

    correction faute commentaire

--- binaire.c
+++ binaire.c
@@ -4,10 +4,10 @@ void binaire(image img,int seuil){
   int y;
   x=img.x;
   y=img.y;
-  for(int i=0; i<x;i++){ // on balye les lignes
-    for(int j=0; j<y;j++){ // on baleye les collones
+  for(int i=0; i<x;i++){ // on balaie les lignes
+    for(int j=0; j<y;j++){ // on balaie les colonnes
       if (((img.pixel.red[i][j]+img.pixel.green[i][j]+img.pixel.blue[i][j])/3)<seuil){
-        img.pixel.red[i][j]=0; // transforme le rouge en 
+        img.pixel.red[i][j]=0; // transforme le rouge dans la bonne teinte
         img.pixel.green[i][j]=0; // transforme le vert dans la bonne teinte
         img.pixel.blue[i][j]=0; // transforme le bleu dans la bonne teinte
       }

427874eeaa9f9dcf0282366e2ce01318d0b252c8
Thomas Girotti <32798532+ThomasGirotti@users.noreply.github.com>
2022-05-24 15:24:19 +0200

    Create import.c

---
+++ import.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void importimage(char* imagename) {
+    int resolution;
+    FILE* file = NULL;
+    char* currentligne[];
+    
+    resolution = x * y;
+  
+  
+    file = fopen("argv[1]","r")
+    if (file == NULL) {
+        printf("Erreur d'ouverture du fichier \n");
+    } else {
+        while (i < resolution) {
+          
+        }
+    }

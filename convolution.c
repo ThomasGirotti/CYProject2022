28bce53bd17382cbb80cae35bd26177a906cf2ea
Lilian <106081036+Sulfy6768@users.noreply.github.com>
2022-05-25 11:13:03 +0200

    Update convolution.c
    codage du squelette de la fonction

--- convolution.c
+++ convolution.c
@@ -1,8 +1,15 @@
-void convultion(image img, int matrice[3][3]){
+void convultion(image img, int** matrice){
   image img2;
-  
-  
-  
-  
-  
+  int compteur;
+  for (int i=0;i<img.x;i++){
+    for (int j=0;j<img.y;j++){
+      compteur=0;
+      for (int k=0;k<2;k++){
+        for (int l=0;l<2;l++){
+          compteur=compteur+matrice[k][l]*img2*[i+k][j+l];
+        }
+      }
+      img2[i][j]=compteur;
+    }
+  } 
 }

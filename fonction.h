e86a6c5ce75902e3fcdc7d779bbd5a05c98c14f8
ThomasGirotti <0022680701v@gmail.com>
2022-05-27 15:39:40 +0200

    Fix image pointer

--- fonction.h
+++ fonction.h
@@ -15,13 +15,13 @@ void importimage(char* inputname,image* im);
 int exportimage(char* outputname,image* im);
 void printimage(image* im);
 
-void binaire(image im,int seuil);
-int* histogramme(image im);
-void miroir(image im);
-void negatif(image im);
-void recadyna(image im);
-void recadrage(image im,int tab[255]);
-image rotate(image im);
-void transform_gris(image im);
+void binaire(image* im,int seuil);
+int* histogramme(image* im);
+void miroir(image* im);
+void negatif(image* im);
+void recadyna(image* im);
+void recadrage(image* im,int tab[255]);
+image rotate(image* im);
+void transform_gris(image* im);
 
 #endif

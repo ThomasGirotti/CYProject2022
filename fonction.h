e52748d88bf9025e1869142e8e9e26c90336baa6
Lilian <106081036+Sulfy6768@users.noreply.github.com>
2022-06-01 14:46:56 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -17,7 +17,7 @@ void printimage(image* im);
 
 void binaire(image* im,int seuil);
 void contraste(image* img);
-void convolution(image* img, int** matrice,image* img2)
+void convolution(image* img, int** matrice,image* img2);
 void histogramme(image* im,int* tab);
 void miroir(image* im);
 void negatif(image* im);
